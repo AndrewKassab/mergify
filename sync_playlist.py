3092ed4e74ea4a7c32147ae8446b900cada2039c
PreciseMotion <andrewkassab@live.com>
2019-09-26 20:24:40 -0700

    added prompt for token

--- sync_playlist.py
+++ sync_playlist.py
@@ -3,3 +3,6 @@ import spotipy
 import spotipy.util as auth
 
 sp = spotipy.Spotify()
+auth.prompt_for_user_token('123881475', 'playlist_modify-private', config.SPOTIPY_CLIENT_ID,
+                           config.SPOTIPY_CLIENT_SECRET, config.SPOTIPY_REDIRECT_URI)
+
