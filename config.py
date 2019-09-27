7e4312a9d0065f393890221ec161bf4b14822d6e
PreciseMotion <andrewkassab@live.com>
2019-09-26 20:20:05 -0700

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+SPOTIPY_CLIENT_ID = ''
+SPOTIPY_CLIENT_SECRET = ''
+SPOTIPY_REDIRECT_URI = ''
+
+playlist_to_sync_into = ""
+playlists_to_sync_from = []
---
+++ sync_playlist.py
@@ -0,0 +1,5 @@
+import config
+import spotipy
+import spotipy.util as auth
+
+sp = spotipy.Spotify()
