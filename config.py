9de5d0ae26180fcf30a0e9d189509e348f095e18
PreciseMotion <andrewkassab@live.com>
2019-09-26 20:42:30 -0700

    successfully retrieving token

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 SPOTIPY_CLIENT_ID = ''
 SPOTIPY_CLIENT_SECRET = ''
-SPOTIPY_REDIRECT_URI = ''
+SPOTIPY_REDIRECT_URI = 'http://localhost:3000/callback/'
 
 playlist_to_sync_into = ""
 playlists_to_sync_from = []
