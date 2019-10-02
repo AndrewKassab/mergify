d47b407f9ba4667bc3467ea9c76e51fe457ab9c3
andrewkassab <andrewkassab@live.com>
2019-10-02 16:14:33 -0700

    put in my test playlist ids

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ SPOTIPY_CLIENT_ID = ''
 SPOTIPY_CLIENT_SECRET = ''
 SPOTIPY_REDIRECT_URI = 'http://localhost:3000/callback/'
 
-USERNAME = ''
+USERNAME = '123881475'
 
-# Use the ID of the playlists, not the names
-DESTINATION_PLAYLIST_ID = ""
-SOURCE_PLAYLIST_IDS = ['5gM1lUohrt2BGcblKezi6h?si=A_f5L_gKQxGgW24zvnhRzQ']
+DESTINATION_PLAYLIST_ID = '1F2mahiwXN6lLuf3bpWE6y?si=ZUu0bdTxQMSGAxUG-CVa_A'
+SOURCE_PLAYLIST_IDS = ['3nWTj1ZAdLJXNiaLdMUOwj?si=0THwLcT5SDujFJhKt5aYIw', '6eNpp03c7e3mnT2Otm7mwT?si=hCeXjEgrQuiXTtVeEtqZLA']
