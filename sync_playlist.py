1952745292b543155ff58241890245e83e72c9b0
andrewkassab <andrewkassab@live.com>
2019-10-02 17:24:06 -0700

    set variables before method calls

--- sync_playlist.py
+++ sync_playlist.py
@@ -2,13 +2,22 @@ import spotipy
 import spotipy.util as auth
 import os
 
-# In your terminal, export these values as described in the readme.
-token = auth.prompt_for_user_token(os.environ['SPOTIFY_USERNAME'], 'playlist-modify-private',
-                                   os.environ['SPOTIFY_CLIENT_ID'], os.environ['SPOTIFY_CLIENT_SECRET'],
-                                   os.environ['SPOTIFY_REDIRECT_URI'])
-
+# Fill These in yourself
 destination_playlist_id = '1F2mahiwXN6lLuf3bpWE6y?si=ZUu0bdTxQMSGAxUG-CVa_A'
 source_playlist_ids = ['3nWTj1ZAdLJXNiaLdMUOwj?si=0THwLcT5SDujFJhKt5aYIw']
+
+scope = 'playlist-read-private playlist-modify-private'
+
+# Export these in your environment
+username = os.environ['SPOTIFY_USERNAME']
+spotify_client_id = os.environ['SPOTIFY_CLIENT_ID']
+spotify_client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
+spotify_redirect_uri = os.environ['SPOTIFY_REDIRECT_URI']
+
+# In your terminal, export these values as described in the readme.
+token = auth.prompt_for_user_token(username, scope, spotify_client_id,
+                                   spotify_client_secret, spotify_redirect_uri)
+
 # other_test_source = '6eNpp03c7e3mnT2Otm7mwT?si=hCeXjEgrQuiXTtVeEtqZLA'
 
 sp = spotipy.Spotify(auth=token)
@@ -16,10 +25,10 @@ sp = spotipy.Spotify(auth=token)
 track_ids = set()
 
 for playlist_id in source_playlist_ids:
-    response = sp.user_playlist_tracks(config.USERNAME, playlist_id, fields='tracks')
+    response = sp.user_playlist_tracks(username, playlist_id, fields='tracks')
     playlist_tracks = response['tracks']['items']
     while len(playlist_tracks) < response['tracks']['total']:
-        response = sp.user_playlist_tracks(config.USERNAME, playlist_id,
+        response = sp.user_playlist_tracks(username, playlist_id,
                                            offset=len(playlist_tracks), fields='tracks')
         playlist_tracks.extend(response['tracks']['items'])
     for item in playlist_tracks:
