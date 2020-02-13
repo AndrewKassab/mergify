c27d24c7ebd16f5384663e58d2c7f0c180e036ed
andrewkassab <andrewkassab@live.com>
2020-02-12 16:23:09 -0800

    adding tracks successful, but adding duplicates too

--- sync_playlist.py
+++ sync_playlist.py
@@ -6,7 +6,7 @@ import os
 
 # Fill These in yourself
 destination_playlist_id = '1F2mahiwXN6lLuf3bpWE6y'
-source_playlist_ids = ['3nWTj1ZAdLJXNiaLdMUOwj']
+source_playlist_ids = ['3nWTj1ZAdLJXNiaLdMUOwj', '6eNpp03c7e3mnT2Otm7mwT']
 
 scope = 'playlist-read-private playlist-modify-private'
 
@@ -20,15 +20,14 @@ spotify_redirect_uri = os.environ['SPOTIFY_REDIRECT_URI']
 token = auth.prompt_for_user_token(username, scope, spotify_client_id,
                                    spotify_client_secret, spotify_redirect_uri)
 
-# other_test_source = '6eNpp03c7e3mnT2Otm7mwT?si=hCeXjEgrQuiXTtVeEtqZLA'
-
 sp = spotipy.Spotify(auth=token)
 
-track_ids = set()
+source_track_ids = set()
+destination_track_ids = set()
 
+# get track ids from source playlists
 for playlist_id in source_playlist_ids:
-    response = sp.playlist_tracks(playlist_id, fields='items.track.id, total')
-    playlist_tracks = response['items']
+    playlist_tracks = []
     while True:
         response = sp.playlist_tracks(playlist_id,
                                            offset=len(playlist_tracks), fields='items.track.id, total')
@@ -36,14 +35,26 @@ for playlist_id in source_playlist_ids:
         if (len(response['items']) == 0):
             break
     for item in playlist_tracks:
-        track_ids.add(item['track']['id'])
+        source_track_ids.add(item['track']['id'])
 
-for track_id in track_ids:
-    print(track_id)
+# get track ids from destination playlist
+destination_playlist_tracks = []
+while True:
+    response = sp.playlist_tracks(playlist_id,
+                                           offset=len(playlist_tracks), fields='items.track.id, total')
+    destination_playlist_tracks.extend(response['items'])
+    if (len(response['items']) == 0):
+        break
+for item in destination_playlist_tracks:
+    destination_track_ids.add(item['track']['id'])
 
-print(len(track_ids))
+track_ids_to_add = list(source_track_ids - destination_track_ids)
 
-
-
-
-
+# add 100 tracks at a time
+start = 0
+while True:
+    track_ids_to_add_shrunk = track_ids_to_add[start:start+100]
+    if (len(track_ids_to_add_shrunk) == 0):
+        break
+    sp.user_playlist_add_tracks(username, destination_playlist_id, track_ids_to_add_shrunk)
+    start = start + 100
