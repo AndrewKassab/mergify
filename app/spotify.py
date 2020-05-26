a64522338c34efe1ebc23b9c4bdc64ce25d4ca51
akassab <andrewkassab@live.com>
2020-05-26 13:48:10 -0700

    finished spotify client tools

--- app/spotify.py
+++ app/spotify.py
@@ -13,7 +13,10 @@ def get_oauth_url():
 
 
 def get_user_playlists(token, username):
-    sp = spotipy.spotify(auth=token)
+    try:
+        sp = spotipy.Spotify(auth=token)
+    except:
+        return -1
     user_playlists_list = []
     offset = 0
     while True:
@@ -26,3 +29,40 @@ def get_user_playlists(token, username):
     for item in user_playlists_list:
         playlists[item['name']] = item['uri'][17:]
     return sp.user_playlists(username)
+
+
+def get_track_ids_from_playlist(playlist_id, token):
+    sp = spotipy.Spotify(auth=token)
+    track_ids = set()
+    playlist_tracks = []
+    while True:
+        response = sp.playlist_tracks(playlist_id, offset=len(playlist_tracks),
+                                      fields='items.track.id, total')
+        playlist_tracks.extend(response['items'])
+        if len(response['items']) == 0:
+            break
+    for item in playlist_tracks:
+        track_ids.add(item['track']['id'])
+    return track_ids
+
+
+def sync_playlists(username, token, source_playlists_ids, destination_playlist_id):
+    try:
+        sp = spotipy.Spotify(auth=token)
+    except:
+        return -1
+    source_track_ids = set()
+    for playlist_id in source_playlists_ids:
+        source_track_ids.union(get_track_ids_from_playlist(playlist_id,token))
+    dest_track_ids = get_track_ids_from_playlist(destination_playlist_id, token)
+    ids_to_add = list(source_track_ids - dest_track_ids)
+
+    offset = 0
+    while True:
+        curr_ids = ids_to_add[offset:offset+100]
+        if len(curr_ids) == 0:
+            break
+        sp.user_playlist_add_tracks(username, destination_playlist_id, curr_ids)
+        offset = offset + 100
+    return 1
+
