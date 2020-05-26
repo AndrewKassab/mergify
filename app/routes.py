3566d017cbb33d8c866039a35cabf8c0b6e59b92
akassab <andrewkassab@live.com>
2020-05-25 21:01:18 -0700

    made get_user_playlists method and most endpoints

--- app/routes.py
+++ app/routes.py
@@ -17,6 +17,28 @@ def login():
         render_template('loginpage.html')
 
 
+@app.route('/auth')
+def spotify_auth():
+    # redirect to my ouath with spotify
+    redirect()
+
+
+@app.route('/callback')
+def callback():
+    if 'error' in request.args:
+        redirect(url_for('login'))
+    auth = request.args['code']
+    response = make_response()
+    response.set_cookie('auth_token', )
+
+
+@app.route('/logout')
+def logout():
+    request.delete_cookie('auth_token')
+    request.delete_cookie('username')
+    redirect(url_for('login'))
+
+
 def is_logged_in(user):
     if 'auth_token' in user.cookies and 'username' in user.cookies:
         return True
---
+++ app/spotify.py
@@ -0,0 +1,23 @@
+import spotipy
+import os
+
+scope = 'playlist-read-private playlist-modify-private'
+client_id = os.environ['PSYNC_CLIENT_ID']
+client_secret = os.environ['PSYNC_CLIENT_SECRET']
+redirect_uri = os.environ['PSYNC_REDIRECT_URI']
+
+
+def get_user_playlists(token, username):
+    sp = spotipy.spotify(auth=token)
+    user_playlists_list = []
+    offset = 0
+    while True:
+        response = sp.user_playlists(username, offset=offset)['items']
+        if len(response) == 0:
+            break
+        user_playlists_list.extend(response)
+        offset = offset + 50
+    playlists = {}
+    for item in user_playlists_list:
+        playlists[item['name']] = item['uri'][17:]
+    return sp.user_playlists(username)
