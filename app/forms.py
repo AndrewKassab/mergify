d5dac6a11bce7eb88679e7aa4115382df8c004d4
akassab <andrewkassab@live.com>
2020-05-31 23:51:33 -0700

    added validation to playlist form

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectMultipleField, SelectField, SubmitField
+from wtforms import validators, ValidationError
 
 
 class PlaylistForm(FlaskForm):
 
     submit_button = SubmitField("Sync")
-    source_playlists = SelectMultipleField("Source Playlists", choices=[])
-    destination_playlist = SelectField("Destination Playlist", choices=[])
+    source_playlists = SelectMultipleField("Source Playlists", validators=[validators.DataRequired(
+        "Please enter at least one source playlist")], choices=[])
+    destination_playlist = SelectField("Destination Playlist", validators=[validators.DataRequired(
+        "Please enter a destination playlist")], choices=[])
