d7b2664d3978de2c9765917613d62ad29d39a380
akassab <andrewkassab@live.com>
2020-07-09 20:40:08 -0700

    spotify utilities unit tests complete

--- app/tests/test_spotify.py
+++ app/tests/test_spotify.py
@@ -10,8 +10,7 @@ playlist_one_name = 'test_one'
 playlist_two_name = 'test_two'
 playlist_one_id = '3Y8wge7Ler8F9PJ2cyBm7S'
 playlist_two_id = '7JdlXBRIdGCi8oOPZGuAeJ'
-playlist_names = {playlist_one_name, playlist_two_name}
-playlist_ids = [playlist_one_id, playlist_two_id]
+playlist_ids = {playlist_one_id, playlist_two_id}
 playlist_one_track_ids = {'2b4SSorCTQ2VzmllaeWuuT', '1JY6B9ILvmRla2IKKRZvnH'}
 playlist_two_track_ids = {'2b4SSorCTQ2VzmllaeWuuT', '41SwdQIX8Hy2u6fuEDgvWr'}
 expected_merge_tracks = {'2b4SSorCTQ2VzmllaeWuuT', '41SwdQIX8Hy2u6fuEDgvWr', '1JY6B9ILvmRla2IKKRZvnH'}
@@ -27,8 +26,8 @@ class SpotifyTest(unittest.TestCase):
 
     def test_get_user_playlists(self):
         playlist_dict = get_user_playlists(token)
-        returned_playlists = set(playlist_dict.keys())
-        self.assertEqual(playlist_names, returned_playlists)
+        returned_playlist_ids = set(playlist_dict.keys())
+        self.assertEqual(playlist_ids, returned_playlist_ids.intersection(playlist_ids))
 
     def test_get_track_ids_from_playlist(self):
         track_ids = get_track_ids_from_playlist(playlist_one_id, token)
@@ -38,8 +37,6 @@ class SpotifyTest(unittest.TestCase):
         merged_playlist_id = merge_to_new_playlist(token, playlist_ids, 'Merge 1/2 Test')
         track_ids = get_track_ids_from_playlist(merged_playlist_id, token)
         self.assertEqual(expected_merge_tracks, track_ids)
-
-
 
 
 if __name__ == '__main__':
