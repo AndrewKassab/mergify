e34b2cf77165ab1f46026d1ca1d6a2b5411677f6
akassab <andrewkassab@live.com>
2020-07-16 18:55:48 -0700

    created login endpoint that creates user on serverside and uses auth_code as auth_token for the app

--- app/db/database.py
+++ app/db/database.py
@@ -39,6 +39,27 @@ class MergifyDataBase:
             return -1
         return rows[0][0]
 
+    def get_user_id_from_username(self, username):
+        con = sql.connect(self.path)
+        cur = con.cursor()
+        cur.execute("SELECT user_id FROM Users WHERE username = '%s'" % username)
+        rows = cur.fetchall()
+        con.close()
+        if len(rows) <= 0:
+            return -1
+        return rows[0][0]
+
+    def update_auth_token_for_user(self, user_id, new_auth_token):
+        con = sql.connect(self.path)
+        cur = con.cursor()
+        cur.execute("SELECT * FROM Users WHERE user_id = '%s'" % user_id)
+        rows = cur.fetchall()
+        if len(rows) <= 0:
+            return -1
+        cur.execute("UPDATE Users SET auth_token = '%s' WHERE user_id = '%s'" % (new_auth_token, user_id))
+        con.commit()
+        con.close()
+
     def get_access_token_for_user(self, user_id):
         return self.__get_tokens_column('access_token', user_id)
 
@@ -83,7 +104,6 @@ class MergifyDataBase:
         rows = cur.fetchall()
         con.close()
         return len(rows) > 0
-
 
     def add_new_entry_to_users(self, username, auth_token, access_token, refresh_token):
         con = sql.connect(self.path)
--- app/routes.py
+++ app/routes.py
@@ -12,30 +12,41 @@ def spotify_auth():
     return redirect(get_oauth_url())
 
 
-# TODO: Rewrite and make to /login
-@app.route('/callback/', methods=['GET'])
-def callback():
-    if 'error' in request.args or 'code' not in request.args:
-        return redirect(url_for('login'))
-    auth_code = request.args['code']
-    token_info = get_token_info_from_code(auth_code)
+@app.route('/login', methods=['POST'])
+def login():
+    code = 201
+
+    try:
+        auth_code = request.form['auth_code']
+        token_info = get_token_info_from_code(auth_code)
+    except Exception as e:
+        payload = {}
+        if type(e) == SpotifyException:
+            code = e.http_status
+            payload['error_detail'] = e.msg
+        else:
+            code = 422
+            payload['error_detail'] = 'Bad Request'
+        return payload, code
+
     access_token = token_info['access_token']
     refresh_token = token_info['refresh_token']
     username = get_username_from_access_token(access_token)
-    response = redirect(url_for('homepage'), 201)
-    response.set_cookie('username', username)
     if db.does_user_exist(username):
-        db.update_access_token_for_user(username, access_token)
-        db.update_refresh_token_for_user(username, refresh_token)
+        user_id = db.get_user_id_from_username()
+        db.update_auth_token_for_user(user_id, auth_code)
+        db.update_access_token_for_user(user_id, access_token)
+        db.update_refresh_token_for_user(user_id, refresh_token)
     else:
-        db.add_new_entry_to_users(username, access_token, refresh_token)
-    return response
+        db.add_new_entry_to_users(username, auth_code, access_token, refresh_token)
+    return code
 
 
 @app.route('/merge', methods=['POST'])
 def merge_playlists():
     code = 201
     payload = {}
+
     auth_token = request.headers.get('auth_token')
     if not is_auth_token_valid(auth_token):
         code = 401
@@ -46,6 +57,7 @@ def merge_playlists():
         token = refresh_and_update_access_token_for_user(user_id)
     else:
         token = db.get_access_token_for_user(user_id)
+
     try:
         source_playlist_ids = [request.form['source_playlists']]
         destination_playlist = request.form['destination_playlist']
@@ -59,7 +71,7 @@ def merge_playlists():
             code = e.http_status
             payload['error_detail'] = e.msg
         else:
-            code = e.code
+            code = 422
             payload['error_detail'] = 'Bad Request'
     return payload, code
 
