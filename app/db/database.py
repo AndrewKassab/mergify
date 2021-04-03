f67419bb668f296839d07c54faf003d8ef649dee
akassab <andrewkassab@live.com>
2021-04-03 14:19:43 -0700

    updated database file for production

--- app/db/database.py
+++ app/db/database.py
@@ -1,28 +1,18 @@
-import sqlite3 as sql
+import mysql.connector
 import os
 import time
 
 token_life = 3000
 db_path = os.path.abspath(os.path.dirname(__file__) + "/database.db")
+db_password = os.environ['MERGIFY_DATABASE_PASSWORD']
 
 
 class MergifyDataBase:
 
-    def __init__(self, path):
-        self.path = path
-        if not os.path.exists(path):
-            con = sql.connect(path)
-            con.execute('CREATE TABLE Users (user_id INTEGER NOT NULL PRIMARY KEY, '
-                        'username TEXT UNIQUE, auth_token TEXT)')
-            con.execute('CREATE TABLE Tokens (user_id INTEGER NOT NULL, access_token TEXT, expiration_time INTEGER, '
-                        'refresh_token TEXT, PRIMARY KEY(user_id), FOREIGN KEY (user_id) REFERENCES Users(user_id))')
-            con.commit()
-            con.close()
-
     def __get_tokens_column(self, column_name, user_id):
-        con = sql.connect(self.path)
+        con = mysql.connector.connect(user='mergify', database='mergify', password=db_password, host='127.0.0.1')
         cur = con.cursor()
-        cur.execute("SELECT %s FROM Tokens WHERE user_id == '%s'" % (column_name, user_id))
+        cur.execute("SELECT %s FROM Tokens WHERE user_id = '%s'" % (column_name, user_id))
         rows = cur.fetchall()
         con.close()
         if len(rows) <= 0:
@@ -30,7 +20,7 @@ class MergifyDataBase:
         return rows[0][0]
 
     def get_user_id_from_auth_token(self, auth_token):
-        con = sql.connect(self.path)
+        con = mysql.connector.connect(user='mergify', database='mergify', password=db_password, host='127.0.0.1')
         cur = con.cursor()
         cur.execute("SELECT user_id FROM Users WHERE auth_token = '%s'" % auth_token)
         rows = cur.fetchall()
@@ -40,7 +30,7 @@ class MergifyDataBase:
         return rows[0][0]
 
     def get_user_id_from_username(self, username):
-        con = sql.connect(self.path)
+        con = mysql.connector.connect(user='mergify', database='mergify', password=db_password, host='127.0.0.1')
         cur = con.cursor()
         cur.execute("SELECT user_id FROM Users WHERE username = '%s'" % username)
         rows = cur.fetchall()
@@ -50,7 +40,7 @@ class MergifyDataBase:
         return rows[0][0]
 
     def update_auth_token_for_user(self, user_id, new_auth_token):
-        con = sql.connect(self.path)
+        con = mysql.connector.connect(user='mergify', database='mergify', password=db_password, host='127.0.0.1')
         cur = con.cursor()
         cur.execute("SELECT * FROM Users WHERE user_id = '%s'" % user_id)
         rows = cur.fetchall()
@@ -70,7 +60,7 @@ class MergifyDataBase:
         return self.__get_tokens_column('expiration_time', user_id)
 
     def __update_tokens_column(self, user_id, column_name, new_value):
-        con = sql.connect(self.path)
+        con = mysql.connector.connect(user='mergify', database='mergify', password=db_password, host='127.0.0.1')
         cur = con.cursor()
         cur.execute("SELECT * FROM Tokens WHERE user_id = '%s'" % user_id)
         rows = cur.fetchall()
@@ -90,7 +80,8 @@ class MergifyDataBase:
         self.__update_tokens_column(user_id, 'refresh_token', new_refresh_token)
 
     def does_user_exist(self, username):
-        con = sql.connect(self.path)
+        con = mysql.connector.connect(user='mergify', database='mergify', password=db_password, host='127.0.0.1')
+        print(con)
         cur = con.cursor()
         cur.execute("SELECT * FROM Users WHERE username = '%s'" % username)
         rows = cur.fetchall()
@@ -98,7 +89,7 @@ class MergifyDataBase:
         return len(rows) > 0
 
     def does_auth_token_exist(self, auth_token):
-        con = sql.connect(self.path)
+        con = mysql.connector.connect(user='mergify', database='mergify', password=db_password, host='127.0.0.1')
         cur = con.cursor()
         cur.execute("SELECT * FROM Users WHERE auth_token = '%s'" % auth_token)
         rows = cur.fetchall()
@@ -106,7 +97,7 @@ class MergifyDataBase:
         return len(rows) > 0
 
     def add_new_entry_to_users(self, username, auth_token, access_token, refresh_token):
-        con = sql.connect(self.path)
+        con = mysql.connector.connect(user='mergify', database='mergify', password=db_password, host='127.0.0.1')
         expiration_time = time.time() + token_life
         cur = con.cursor()
         cur.execute("INSERT INTO Users (username, auth_token) VALUES ('%s','%s')" % (username, auth_token))
@@ -114,6 +105,6 @@ class MergifyDataBase:
                     "('%s','%s','%s')" % (access_token, expiration_time, refresh_token))
         con.commit()
         con.close()
+    
 
-
-db = MergifyDataBase(db_path)
+db = MergifyDataBase()
