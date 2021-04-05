c607f2d59c5f677573431c2b7b4a335d85dfb59a
andrewkassab <andrewkassab@live.com>
2021-04-04 18:04:58 -0700

    fixed typo in schema

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,2 +1,2 @@
 CREATE TABLE Users (user_id INTEGER NOT NULL PRIMARY KEY, username VARCHAR(100) UNIQUE, auth_token VARCHAR(500));
-CREATE TABLE TOkens (user_id INTEGER NOT NULL, access_token TEXT, expiration_time INTEGER, refresh_token VARCHAR(500), PRIMARY KEY(user_id), FOREIGN KEY (user_id) REFERENCES Users(user_id));
+CREATE TABLE Tokens (user_id INTEGER NOT NULL, access_token TEXT, expiration_time INTEGER, refresh_token VARCHAR(500), PRIMARY KEY(user_id), FOREIGN KEY (user_id) REFERENCES Users(user_id));
